33b8c3de629a51e4d3f0228d88cff8f70fea1202
wojtek-krysiak <wojciech.g.krysiak@gmail.com>
2020-07-26 17:35:49 +0200

    fix: export carbon icons and datepicker from bundle

--- entry.js
+++ entry.js
@@ -1,6 +1,11 @@
+import ReactDatepicker from 'react-datepicker'
+import CarbonIcons from '@carbon/icons-react'
+
 import * as DesignSystem from './src/index'
 
 window.AdminBro = window.AdminBro || {}
 window.AdminBro.DesignSystem = DesignSystem
+window.ReactDatepicker = window.ReactDatepicker || ReactDatepicker
+window.CarbonIcons = window.CarbonIcons || CarbonIcons
 
 export default DesignSystem
