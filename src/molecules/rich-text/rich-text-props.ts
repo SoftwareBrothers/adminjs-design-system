f2e7ce657f09444d5f312deeb69a22ec40730eca
Wojciech <wojciech.g.krysiak@gmail.com>
2020-10-01 18:20:44 +0200

    docs: update docs to work with better-docs

--- src/molecules/rich-text/rich-text-props.ts
+++ src/molecules/rich-text/rich-text-props.ts
@@ -14,7 +14,29 @@ export type QuillToolbarOptions = Array<
 >
 
 /**
- * Prop Types of a a Quill Toolbar
+ * variable holding a default quill toolbar. It looks like this:
+ * 
+ * ```javascript
+ * export const DefaultQuillToolbarOptions = [
+ *   [{ header: [1, 2, 3, 4, 5, 6, false] }],
+ *   ['bold', 'italic', 'underline', 'strike'], // toggled buttons
+ *   ['blockquote', 'code-block'],
+ *   [{ list: 'ordered' }, { list: 'bullet' }],
+ *   [{ indent: '-1' }, { indent: '+1' }],
+ * 
+ *   [{ align: [] }],
+ *   ['link', 'image', 'video'],
+ * 
+ *   ['clean'], // remove formatting button
+ * ]
+ * ```
+ * 
+ * ### Usage
+ * 
+ * ```javascript
+ * import { DefaultQuillToolbarOptions } from '@admin-bro/design-system`
+ * ```
+ * 
  *
  * @memberof RichText
  * @alias DefaultQuillToolbarOptions
--- src/utils/theme-get.ts
+++ src/utils/theme-get.ts
@@ -3,36 +3,6 @@ import { DefaultTheme } from 'styled-components'
 
 type Kind = keyof DefaultTheme
 
-/**
- *
- * Handy utility function which simplifies taking values from the {@link Theme}.
- *
- * ### Example
- *
- * ```javascript
- * import { Box, themeGet } from '@admin-bro/design-system'
- *
- * const myComponent = styled(Box)`
- *   padding: ${themeGet('space', 'xl')};
- *   font: ${themeGet('font')};
- *   margin-top: ${themeGet('space', 'xl', -2)};
- *
- *   margin-bottom: ${(props) => themeGet('space', props.disabled ? 'xl' : 'xxl')(props)};
- * `
- *
- * ```
- *
- * @param {string} kind           One of the keys in the {@link Theme} interface. For example:
- *                                `fontWeights`
- * @param {string} [value]        Value for the key
- * @param {string | number} [offset]    optional offset. When you want to take xxl + 2pixels
- *                                you can do this by passing +2 as the offset.
- * @return {ThemeFunction}  function taking props as an argument
- *
- * @memberof module:@admin-bro/design-system
- * @new in version 3.3
- * @function themeGet
- */
 function themeGet<T extends Kind>(
   kind: T,
   value?: keyof DefaultTheme[T],
@@ -60,3 +30,34 @@ export {
   themeGet as default,
   themeGet,
 }
+
+/**
+ *
+ * Handy utility function which simplifies taking values from the {@link Theme}.
+ *
+ * ### Example
+ *
+ * ```javascript
+ * import { Box, themeGet } from '@admin-bro/design-system'
+ *
+ * const myComponent = styled(Box)`
+ *   padding: ${themeGet('space', 'xl')};
+ *   font: ${themeGet('font')};
+ *   margin-top: ${themeGet('space', 'xl', -2)};
+ *
+ *   margin-bottom: ${(props) => themeGet('space', props.disabled ? 'xl' : 'xxl')(props)};
+ * `
+ *
+ * ```
+ *
+ * @param {string} kind           One of the keys in the {@link Theme} interface. For example:
+ *                                `fontWeights`
+ * @param {string} [value]        Value for the key
+ * @param {string | number} [offset]    optional offset. When you want to take xxl + 2pixels
+ *                                you can do this by passing +2 as the offset.
+ * @return {ThemeFunction}  function taking props as an argument
+ *
+ * @new in version 3.3
+ * @function themeGet
+ * @memberof Theme
+ */
