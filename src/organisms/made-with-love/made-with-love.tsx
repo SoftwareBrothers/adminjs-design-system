1e2b869cef84f5b0fb571f4206946f42bf081440
Arian Sobczak <85543813+ariansobczak-rst@users.noreply.github.com>
2022-03-23 08:49:29 +0100

    feat: Rebrand (#33)
    * feat: added made with love label
    
    BREAKING CHANGE: removed SoftwareBrothersLogo component and introduced MadeWithLove

---
+++ src/organisms/made-with-love/made-with-love.tsx
@@ -0,0 +1,44 @@
+import React, { FC } from 'react'
+import styled from 'styled-components'
+import { Box, Icon, Link, Text } from '../..'
+import { themeGet } from '../../utils'
+
+const StyledWrapper = styled(Box)`
+  user-select: none;
+  & > * {
+    padding: 0 ${themeGet('space', 'xs')};
+  }
+`
+
+StyledWrapper.defaultProps = {
+  color: 'grey60',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+}
+
+const StyledLink = styled(Link)`
+  font-size: ${themeGet('fontSizes', 'sm')};
+  font-weight: 300;
+  &:hover {
+    color: ${themeGet('colors', 'love')};
+    text-decoration: none;
+  }
+`
+
+const MadeWithLove: FC = () => (
+  <StyledWrapper>
+    <Text as="span" variant="sm">
+      Made with
+    </Text>
+    <Icon icon="FavoriteFilled" color="love" />
+    <Text as="span" variant="sm">
+      by
+    </Text>
+    <StyledLink href="https://adminjs.co/" target="_blank" rel="noopener noreferrer">
+      AdminJS Team
+    </StyledLink>
+  </StyledWrapper>
+)
+
+export default MadeWithLove
