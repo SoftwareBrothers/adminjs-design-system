d1469170408cdce83ee44f559382246df85cdab8
Arian Sobczak <arian.sobczak@rst.com.pl>
2022-03-28 10:04:28 +0200

    chore: removed quill, fixed tooltip content export

--- entry.js
+++ entry.js
@@ -1,11 +1,9 @@
 import ReactDatepicker from 'react-datepicker'
 import * as CarbonIcons from '@carbon/icons-react'
-import Quill from 'quill'
 
 import * as AdminJSDesignSystem from './src/index'
 
 window.ReactDatepicker = window.ReactDatepicker || ReactDatepicker
 window.CarbonIcons = window.CarbonIcons || CarbonIcons
-window.Quill = Quill
 
 export default AdminJSDesignSystem
