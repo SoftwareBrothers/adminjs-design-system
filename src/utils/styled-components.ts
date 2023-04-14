4ac1b38306a35df2eba97e74e9e450c511b9ecc3
Rafal Dziegielewski <rafal.dzieg@gmail.com>
2023-04-14 09:22:07 +0200

    fix(beta): make explicit exports from styled-components

--- src/utils/styled-components.ts
+++ src/utils/styled-components.ts
@@ -1,9 +1,9 @@
 import * as _styled from 'styled-components'
+import { ThemeProvider, css, createGlobalStyle, type DefaultTheme, keyframes, useTheme } from 'styled-components'
 
 export * from 'styled-components'
 
 const defStyled = _styled.default ?? _styled
 const styled = defStyled?.default ?? defStyled
 
-export { styled }
-export default styled
+export { styled, ThemeProvider, css, createGlobalStyle, DefaultTheme, keyframes, useTheme }
