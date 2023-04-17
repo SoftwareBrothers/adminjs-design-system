654772ac1429e48a060159444c62c1aef7b6ec80
Rafal Dziegielewski <rafal.dzieg@gmail.com>
2023-04-17 16:20:46 +0200

    chore(beta): fix import

--- entry.js
+++ entry.js
@@ -2,12 +2,9 @@ import ReactDatepicker from 'react-datepicker'
 import * as FeatherIcons from 'react-feather'
 
 import * as AdminJSDesignSystem from './build/index.js'
-import * as styled from './build/utils/styled-components.js'
+import * as styled from './build/utils/styled.js'
 
 window.styled = window.styled || styled
-if (!window.styled.styled && window.styled.default) {
-  window.styled.styled = window.styled.default
-}
 window.ReactDatepicker = window.ReactDatepicker || ReactDatepicker.default || ReactDatepicker
 window.FeatherIcons = window.FeatherIcons || FeatherIcons
 
