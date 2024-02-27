b874d1d842c6c4977cf933d46ec176ddbe283be8
Rafal Dziegielewski <rafal.dzieg@gmail.com>
2024-02-27 22:13:13 +0100

    fix: fix how parseISO is imported from date-fns

--- src/molecules/date-picker/useDatePicker.ts
+++ src/molecules/date-picker/useDatePicker.ts
@@ -1,5 +1,6 @@
 import { useState } from 'react'
-import parseISO from 'date-fns/parseISO'
+
+import parseISO from 'date-fns/parseISO/index.js'
 
 import { formatDateProperty, PropertyType } from '../../utils/index.js'
 
